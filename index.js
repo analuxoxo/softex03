d3ba92f30d7f030398950c37c6f1a2be06fd955e
unknown <rafabruna.santana@gmail.com>
2025-08-12 16:44:07 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let n1 = 8
+let n2 = 10
+let n3 = 7
+let n4 = 8
+//a media do aluno
+let media = (n1+n2+n3+n4)/4
+//mostrar a media do aluno
+console.log(media)
+
+let numero = Math.sqrt(9)
+
+console.log(numero)
+
+let a = 2
+let b = 3
+let c = 5
+
+let beta = ((b+b))-((4*a*c))
+
+let raiz = Match.sqrt(beta)
+
+let x1 = -(b + Match.sqrt(b**2-a*b))/2*a
+let x2 = -(b - Match.sqrt(b**2-a*b))/2*a
+
+console.log(raiz)
+console.log(x1)
+console.log(x2)
+
